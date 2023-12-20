961a7405a232dde064cc3b00af65944140a3220b
PhuocBuiCong <phuocbuii1999@gmail.com>
2023-12-20 23:00:00 +0700

    initialize

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import Icon from "./components/atoms/Icon.vue";
+</script>
+
+<template>
+  <p class="text-red-500">hello tiktok</p>
+  <Icon icon-name="fa-search" size="2xl" />
+</template>
---
+++ src/components/atoms/Icon.vue
@@ -0,0 +1,20 @@
+<!-- Copyright 2022 Credit Engine, Inc. -->
+<script setup lang="ts">
+import { watch } from "vue";
+
+export interface Props {
+  size: string;
+  color: string;
+  iconName: string;
+}
+// export interface Emits {
+//   (e: "update:modelValue", value: string): void;
+// }
+// const emit = defineEmits<Emits>();
+
+const { size, color, iconName } = defineProps<Props>();
+</script>
+
+<template>
+  <font-awesome-icon :size="size" :icon="iconName" />
+</template>
